2d0982a2c2b7830f9e97a06ad619cbed1f2671cf
Aeon <CYH107015@163.com>
2024-02-05 23:34:35 +0800

    美赛修正

--- 2024美赛/own_code/第二题2.py
+++ 2024美赛/own_code/第二题2.py
@@ -6,7 +6,7 @@ import random
 import math
 
 np.random.seed(43)
-a = stats.norm.rvs(525, 350, 100)  # 随机生态位
+a = stats.norm.rvs(650, 100, 100)  # 随机生态位
 print(a)
 
 total_t = 1000
@@ -27,10 +27,10 @@ for k in range(100):
     f_2[0] = 10
 
     for t in range(1, total_t):
-        m_1[t] = m_1[t - 1] + 0.2 * m_1[t - 1] * (1 - (m_1[t - 1] + 0.1 * f_1[t - 1])/a[k])
-        f_1[t] = f_1[t - 1] + 0.2 * f_1[t - 1] * (1 - (0.3 * m_1[t - 1] + f_1[t - 1])/a[k])
-        m_2[t] = m_2[t - 1] + 0.4 * m_2[t - 1] * (1 - (m_2[t - 1] + f_2[t - 1]) / a[k])
-        f_2[t] = f_2[t - 1] + 0.5 * f_2[t - 1] * (1 - (m_2[t - 1] + f_2[t - 1]) / a[k])
+        m_1[t] = m_1[t - 1] + 0.6 * m_1[t - 1] * (1 - (m_1[t - 1] + 0.65 * f_1[t - 1]) / a[k])
+        f_1[t] = f_1[t - 1] + 0.5 * f_1[t - 1] * (1 - (0.9 * m_1[t - 1] + f_1[t - 1]) / a[k])
+        m_2[t] = m_2[t - 1] + 0.95 * m_2[t - 1] * (1 - (m_2[t - 1] + f_2[t - 1]) / a[k])
+        f_2[t] = f_2[t - 1] + 0.85 * f_2[t - 1] * (1 - (m_2[t - 1] + f_2[t - 1]) / a[k])
         if m_1[t]< 0:
             m_1[t] = 0
         if f_1[t] < 0:
@@ -53,8 +53,8 @@ for i in range(100):
     population2_result[i] = m2_total[i][-1] + f2_total[i][-1]
 
 plt.figure()
-plt.plot(population1_result, label='population 1')
-plt.plot(population2_result, label='population 2')
+plt.plot(population1_result, label='lamprey')
+plt.plot(population2_result, label='bio B')
 plt.legend(loc='upper right')
 plt.show()
 print(population1_result)
