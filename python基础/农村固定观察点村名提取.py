af9363d44e2b48ead8e6ef7fa95fa2542cd04378
Aeon <CYH107015@163.com>
2024-03-22 22:52:38 +0800

    新增

--- python基础/农村固定观察点村名提取.py
+++ python基础/农村固定观察点村名提取.py
@@ -1,5 +1,12 @@
 import pandas as pd
+import jionlp as jio
 
 data = pd.read_excel("C:\\Users\\CYH10\\Desktop\\村名.xlsx")
-
-
+df = pd.DataFrame(data)
+for i in range(len(data['村名'])):
+    res = jio.parse_location(data['村名'][i])
+    if res != "":
+        df.loc[i, '县名'] = res['county']
+    if res == "":
+        df.loc[i, '县名'] =
+df.to_excel("C:\\Users\\CYH10\\Desktop\\村名_adj.xlsx")
