c063996131008b6801f4ca71ecd9b24ac47286e1
Aeone <cyh107015@outlook.com>
2026-03-01 16:51:57 +0800

    del blank document

--- Case/wenshuwang/spider.py
+++ Case/wenshuwang/spider.py
@@ -1,7 +1,10 @@
+import re
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.select import Select
 import time
+from twocaptcha import TwoCaptcha
+import pandas as pd
 
 # 初始化selenium
 url = 'https://wenshu.court.gov.cn/website/wenshu/181029CR4M5A62CH/index.html?'
@@ -21,13 +24,13 @@ option.add_experimental_option('prefs', prefs)
 driver = webdriver.Chrome(options=option)
 # 设置打开的浏览器窗口最大化
 driver.maximize_window()
-driver.set_page_load_timeout(30)
+# driver.set_page_load_timeout(30)
 driver.get(url)
 
 # 转到登录界面自动输入手机号密码进行登录
 driver.find_element(By.XPATH, '//*[@id="loginLi"]/a').click()
 text = driver.page_source
-time.sleep(10)  # 等待页面渲染
+time.sleep(20)  # 等待页面渲染
 # 进入iframe框
 iframe = driver.find_elements(By.TAG_NAME, 'iframe')[0]
 driver.switch_to.frame(iframe)
@@ -36,16 +39,28 @@ driver.switch_to.frame(iframe)
 username = driver.find_element(
     By.XPATH, '//*[@id="root"]/div/form/div/div[1]/div/div/div/input')
 username.send_keys('17855107615')
-
-time.sleep(3)
+time.sleep(2)
 
 username = driver.find_element(
-    By.XPATH, '//*[@id="root"]/div/form/div/div[2]/div/div/div/input')
+    By.XPATH, '//*[@id="root"]/div/form/div/div[2]/div/div/div/input'
+)
 username.send_keys('Caoyihan7015!')
-
 time.sleep(2)
+
+captcha_image = driver.find_element(
+    By.XPATH, '//*[@id="root"]/div[1]/form/div/div[3]/div/div/div/img')
+captcha_image.screenshot('captcha.png')
+api_key = '8ef4d5850d970790349c9c78831adb21'
+solver = TwoCaptcha(api_key)
+text = solver.normal('captcha.png')
+result = text['code']
+username = driver.find_element(
+    By.XPATH, '//*[@id="root"]/div[1]/form/div/div[3]/div/div/div/input')
+username.send_keys(result)
+time.sleep(2)
+
 driver.find_element(
-    By.XPATH, '//*[@id="root"]/div/form/div/div[3]/span').click()
+    By.XPATH, '//*[@id="root"]/div[1]/form/div/div[4]/span').click()
 time.sleep(3)
 
 # 必须加上表单退出，否者就是死元素无法定位
@@ -61,42 +76,35 @@ time.sleep(10)
 _lastWindow = driver.window_handles[-1]
 driver.switch_to.window(_lastWindow)
 
-# 选择案件批量下载
+"""
+按条件检索
+"""
+# 点击高级检索
+driver.find_element(
+    By.XPATH, '//*[@id="_view_1545034775000"]/div/div[1]/div[1]').click()
+# 设置检索条件
+# 地点
+place = driver.find_element(By.XPATH, '//*[@id="s2"]')
+place.send_keys('长丰县')
+time.sleep(2)
+# 程序
+driver.find_element(By.XPATH, '//*[@id="s9"]').click()
+driver.find_element(By.XPATH, '//*[@id="0201_anchor"]').click()
+# time
+time_begin = driver.find_element(By.XPATH, '//*[@id="cprqStart"]')
+time_begin.send_keys('2019-01-01')
+time_end = driver.find_element(By.XPATH, '//*[@id="cprqEnd"]')
+time_end.send_keys('2019-12-31')
 
-# 这行代码的作用就相当于你手动点了一下‘法院层级’那个按钮
-# driver.find_element(By.LINK_TEXT, '法院层级').click()
-
-# 按照裁判日期排序显示最新600条
-# driver.find_element(By.LINK_TEXT, '裁判日期').click()
-# 按照裁判日期从前到后显示最老600条
-# driver.find_element(By.LINK_TEXT, '裁判日期').click()
-
-# 按照审判程序排序显示最新600条
-# driver.find_element(By.LINK_TEXT, '审判程序').click()
-# 按照审判程序从前到后显示最老600条
-# driver.find_element(By.LINK_TEXT, '审判程序').click()
-
-# 按照法院层级、地域及法院进行检索
-driver.find_element(By.LINK_TEXT, '高级法院(146202)').click()
-time.sleep(3)
-driver.find_element(By.LINK_TEXT, '上海市(1188)').click()
-
-# 在搜索框输入关键词进行高级检索
-# 定位到搜索框
-keyword = driver.find_element(
-    By.XPATH, '//*[@id="_view_1545034775000"]/div/div[1]/div[2]/input')
-time.sleep(3)
-# 输入高级检索关键词，例如 工程纠纷
-keyword.send_keys('工程纠纷')
-time.sleep(3)
 # 点击搜索按钮
 driver.find_element(
-    By.XPATH, '//*[@id="_view_1545034775000"]/div/div[1]/div[3]').click()
+    By.XPATH, '//*[@id="searchBtn"]').click()
 
 # 将每页文件数设置为最大,15条
-page_size_box = Select(driver.find_element(
-    By.XPATH, '//*[@id="_view_1545184311000"]/div[8]/div/select'))
-page_size_box.select_by_visible_text('15')
+page_size_box = driver.find_element(
+    By.XPATH, '//*[@id="_view_1545184311000"]/div[8]/div/select').click()
+page = driver.find_element(
+    By.XPATH, '//*[@id="_view_1545184311000"]/div[8]/div/select/option[3]').click()
 
 
 def test_exceptions(xpath):
@@ -107,29 +115,110 @@ def test_exceptions(xpath):
         return False
 
 
-page = 1
-# 最多显示600条文件,也就是40页
-while page <= 40:
-    time.sleep(5+page/10)
-    for i in range(15):
-        time.sleep(5+i/10)
-        event_xpath = '//*[@id="_view_1545184311000"]/div[' + \
-            str(i+3) + ']/div[6]/div/a[2]'
-        if test_exceptions(event_xpath) == True:
-            driver.find_element(By.XPATH, event_xpath).click()
-        else:
-            event_xpath = '//*[@id="_view_1545184311000"]/div[' + \
-                str(i+3) + ']/div[5]/div/a[2]'
-            if test_exceptions(event_xpath) == True:
-                driver.find_element(By.XPATH, event_xpath).click()
+# 转为dataframe,设置行标题
+data = []
+data = pd.DataFrame(
+    data, columns=['标题', '案由', '案号', '法院', '裁判日期', '当事人', '案件正文'])
 
-    # 下一页按钮,不能用Xpath定位,因为“下一页”按钮位置不固定
-    # driver.find_element(By.LINK_TEXT, '下一页').click()
-    time.sleep(5)
-    driver.find_element(By.LINK_TEXT, '下一页').click()
-    # 必须加上表单退出，否者就是死元素无法定位
-    driver.switch_to.default_content()
-    page += 1
+event_xpath = '//*[@id="_view_1545184311000"]/div[3]/div[2]/h4/a'
+# if test_exceptions(event_xpath) == True:
+driver.find_element(By.XPATH, event_xpath).click()
+# 切换到新窗口
+_lastWindow = driver.window_handles[-1]
+driver.switch_to.window(_lastWindow)
+driver.find_element(
+    By.XPATH, '//*[@id="_view_1541573889000"]/div[1]/div[1]/div[1]/img').click()
+# 保存制定内容
+# 标题
+title = driver.find_element(
+    By.XPATH, '//*[@id="_view_1541573883000"]/div/div[1]/div[1]').text
+# 案由
+case_reason = driver.find_element(
+    By.XPATH, '//*[@id="iframedf"]/span[1]').text
+# 案号
+case_number = driver.find_element(
+    By.XPATH, '//*[@id="iframedfah"]/span[1]').text
+# 法院
+court = driver.find_element(
+    By.XPATH, '//*[@id="_view_1541573889000"]/div[1]/div[1]/div[2]/div[2]/ul/li[1]/h4[1]/a').text
+# 裁判日期
+judgment_date = driver.find_element(
+    By.XPATH, '//*[@id="_view_1541573889000"]/div[1]/div[1]/div[2]/div[2]/ul/li[1]/h4[5]/b').text
+# 当事人
+party = driver.find_element(
+    By.XPATH, '//*[@id="_view_1541573889000"]/div[1]/div[1]/div[2]/div[2]/ul/li[1]/h4[6]/b').text
+# 案件正文
+content = driver.find_element(
+    By.XPATH, '//*[@id="2"]').text
+data.loc[len(data)] = [title, case_reason, case_number, court,
+                       judgment_date, party, content]
 
-# 关闭整个浏览器窗口并终止与浏览器的会话
-driver.quit()
+print(data)
+# page = 1
+# # 最多显示600条文件,也就是40页
+# while page <= 40:
+#     time.sleep(5+page/10)
+#     for i in range(15):
+#         time.sleep(5+i/10)
+#         # 进入文书内部
+#         event_xpath = '//*[@id="_view_1545184311000"]/div[' + \
+#             str(i+3)+']/div[3]/div[2]/h4/a'
+#         if test_exceptions(event_xpath) == True:
+#             driver.find_element(By.XPATH, event_xpath).click()
+#             # 切换到新窗口
+#             _lastWindow = driver.window_handles[-1]
+#             driver.switch_to.window(_lastWindow)
+#             driver.find_element(
+#                 By.XPATH, '//*[@id="_view_1541573889000"]/div[1]/div[1]/div[1]/img').click()
+#             # 保存制定内容
+#             # 标题
+#             title = driver.find_element(
+#                 By.XPATH, '//*[@id="_view_1541573883000"]/div/div[1]/div[1]').text
+#             # 案由
+#             case_reason = driver.find_element(
+#                 By.XPATH, '//*[@id="iframedf"]/span[1]').text
+#             # 案号
+#             case_number = driver.find_element(
+#                 By.XPATH, '//*[@id="iframedfah"]/span[1]').text
+#             # 法院
+#             court = driver.find_element(
+#                 By.XPATH, '//*[@id="_view_1541573889000"]/div[1]/div[1]/div[2]/div[2]/ul/li[1]/h4[1]/a').text
+#             # 裁判日期
+#             judgment_date = driver.find_element(
+#                 By.XPATH, '//*[@id="_view_1541573889000"]/div[1]/div[1]/div[2]/div[2]/ul/li[1]/h4[5]/b').text
+#             # 当事人
+#             party = driver.find_element(
+#                 By.XPATH, '//*[@id="_view_1541573889000"]/div[1]/div[1]/div[2]/div[2]/ul/li[1]/h4[6]/b').text
+#             # 案件正文
+#             content = driver.find_element(
+#                 By.XPATH, '//*[@id="2"]').text
+#             data.loc[len(data)] = [title, case_reason, case_number, court,
+#                                    judgment_date, party, content]
+#             # 关闭当前窗口
+#             driver.find_element(By.XPATH, '//*[@id="btnClose"]').click()
+#         else:
+#             event_xpath = '//*[@id="_view_1545184311000"]/div[' + \
+#                 str(i+3) + ']/div[5]/div/a[2]'
+#             if test_exceptions(event_xpath) == True:
+#                 driver.find_element(By.XPATH, event_xpath).click()
+
+#         # event_xpath = '//*[@id="_view_1545184311000"]/div[' + \
+#         #     str(i+3) + ']/div[6]/div/a[2]'
+#         # if test_exceptions(event_xpath) == True:
+#         #     driver.find_element(By.XPATH, event_xpath).click()
+#         # else:
+#         #     event_xpath = '//*[@id="_view_1545184311000"]/div[' + \
+#         #         str(i+3) + ']/div[5]/div/a[2]'
+#         #     if test_exceptions(event_xpath) == True:
+#         #         driver.find_element(By.XPATH, event_xpath).click()
+
+#     # 下一页按钮,不能用Xpath定位,因为“下一页”按钮位置不固定
+#     # driver.find_element(By.LINK_TEXT, '下一页').click()
+#     time.sleep(5)
+#     driver.find_element(By.LINK_TEXT, '下一页').click()
+#     # 必须加上表单退出，否者就是死元素无法定位
+#     driver.switch_to.default_content()
+#     page += 1
+
+# # # 关闭整个浏览器窗口并终止与浏览器的会话
+# # driver.quit()
--- test.py
+++ test.py
@@ -1,12 +1,6 @@
-from scipy.stats import ttest_ind
-import numpy as np
+from twocaptcha import TwoCaptcha
 
-# 创建两组数据
-data1 = np.random.normal(0, 1, 100)
-data2 = np.random.normal(1, 1, 100)
-
-# 使用 ttest_ind 进行独立样本 t 检验
-t_value, p_value = ttest_ind(data1, data2)
-
-# 只显示 t 值
-print("t 值:", t_value)
+api_key = '8ef4d5850d970790349c9c78831adb21'
+solver = TwoCaptcha(api_key)
+result = solver.normal('captcha.png')
+print(result['code'])
